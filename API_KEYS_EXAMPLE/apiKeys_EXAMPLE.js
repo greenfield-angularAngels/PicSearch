b72a44a81a76a1a832a89a637fcc07e7076d5622
sean0923 <iamseanhong@gmail.com>
2017-08-22 00:09:09 -0700

    Add API_KEYS_EXAMPLE folder

---
+++ API_KEYS_EXAMPLE/apiKeys_EXAMPLE.js
@@ -0,0 +1,13 @@
+
+// PUT YOUR API KEYS AND CHANGE FOLDER NAME TO API_KEYS instead of API_KEYS_EXAMPLE
+// get your own gVisionJsonApi.json file from google api
+// change the name of that .json file to gVisionJsonApi.json instead of gVisionJsonApi_EXAMPLE.json
+
+
+// const bingAccKeys = 'a44aa769c3534dd284dbcf271bf037de';
+const bingAccKeys = 'YOUR_BING_API_KEY';
+const gVisionProjectId = 'YOUR_G_VISION_PROJECT_ID';
+
+module.exports.bingAccKeys = bingAccKeys;
+module.exports.gVisionProjectId = gVisionProjectId;
+
